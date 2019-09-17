3b2a36fd42091e5102b6855ff5be8300587863fe
Gavin Kimpson <gkimpson@gmail.com>
2019-09-17 22:47:48 +0100

    routing to controllers

--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts/{post}', function ($post) {
-    $posts = [
-        'my-first-post' => 'Hello this is my first post',
-        'my-second-post' => 'Now I am getting the hang of this blogging'
-    ];
-
-    if (! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found');
-    }
-
-    return view('post', [
-        'post' => $posts[$post]
-     ]);
-});
-
-Route::get('/test', function () {
-    return view('test', [
-       'name' => request('name')
-    ]);
-});
+Route::get('/posts/{post}', 'PostsController@show');
